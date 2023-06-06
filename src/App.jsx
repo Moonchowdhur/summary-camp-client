ef8bdb5280a870a9656a3bab8e9588fea7db87e9
kaushani <kaushanichowdhury52@gmail.com>
2023-06-06 21:50:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="text-center">
+      <button className=" text text-2xl btn btn-accent">Button</button>
+    </div>
+  );
+};
+
+export default App;
