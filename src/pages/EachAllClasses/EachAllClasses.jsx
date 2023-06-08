b325e56319e817f9a961601d9f29fe2a34335bea
kaushani <kaushanichowdhury52@gmail.com>
2023-06-08 08:52:16 +0530

    added

--- src/pages/EachAllClasses/EachAllClasses.jsx
+++ src/pages/EachAllClasses/EachAllClasses.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { MdEventSeat } from "react-icons/md";
 
 const EachAllClasses = ({ c }) => {
   const {
@@ -11,9 +12,14 @@ const EachAllClasses = ({ c }) => {
     seats,
     status,
   } = c;
+
+  const selectClassBtn = (id) => {
+    console.log(id);
+  };
+
   console.log(c);
   return (
-    <div className="shadow-lg relative p-4 shadow-slate-100 border-2 border-violet-500 rounded-lg">
+    <div className="shadow-lg relative p-4 shadow-slate-100 border-2 border-violet-600 rounded-lg">
       <div>
         <img
           src={image}
@@ -24,13 +30,25 @@ const EachAllClasses = ({ c }) => {
           <p className="text-2xl tracking-wider font-medium uppercase ">
             {name}
           </p>
-          <p className="text-base bg-violet-500 text-white px-2 py-3 rounded-lg font-medium absolute top-3 right-5">
+          <p className="text-base bg-violet-600 text-white px-2 py-3 rounded-lg font-medium absolute top-3 right-5">
             Price: ${price}
           </p>
 
           <p className="text-xl tracking-wider font-medium my-3">
             Instructor Name:{instructorName}
           </p>
+          <div className="flex gap-8 w-full  items-center">
+            <p className="text-lg flex items-center gap-2 font-medium">
+              <MdEventSeat className="text-2xl text-violet-600" />
+              Available seats: {seats}
+            </p>
+            <button
+              onClick={() => selectClassBtn(_id)}
+              className="rounded-lg text-xl bg-violet-600 px-3 py-2 text-white font-bold"
+            >
+              Select
+            </button>
+          </div>
         </div>
       </div>
     </div>
